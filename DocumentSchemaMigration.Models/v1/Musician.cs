3a13acd3b16f026f559acebe7db690a3fd4db5c2
Michał Depta <mdepta@future-processing.com>
2017-09-19 12:18:16 +0200

    Added initial data model

---
+++ DocumentSchemaMigration.Models/v1/Musician.cs
@@ -0,0 +1,30 @@
+﻿namespace DocumentSchemaMigration.Models.v1
+{
+    public class Musician
+    {
+        public Musician(string id, string firstName, string lastName, Intrument intrument)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Intrument = intrument;
+        }
+
+        public string Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public Intrument Intrument { get; }
+    }
+
+    public enum Intrument
+    {
+        Vocals,
+        Guiter,
+        Bass,
+        Drums,
+        Keyboard
+    }
+}
